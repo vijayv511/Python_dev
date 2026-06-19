0cc26841c6e56d1d22d034dbb6d9813098255206
vijayv511 <88069606+vijayv511@users.noreply.github.com>
2026-06-19 13:13:42 +0530

    Create app.py

---
+++ src/myapi/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Python API is running",
+        "timestamp": datetime.now().strftime("%H:%M:%S %d %b %Y")
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "UP"
+    })
+
+
+def main():
+    app.run(host="0.0.0.0", port=5000)
+
+
+if __name__ == "__main__":
+    main()
